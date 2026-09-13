9b856fc41ca16957b8ae18b3d7159f7ee59a236e
Shravani Budharam <shravanibudharam25@gmail.com>
2026-09-13 21:07:56 +0530

    Add VPC module configuration in main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,20 @@
+module "vpc" {
+  source = "./modules/vpc"
+
+  project_name = var.project_name
+
+  availability_zones = [
+    "us-east-1a",
+    "us-east-1b"
+  ]
+
+  public_subnet_cidrs = [
+    "10.0.1.0/24",
+    "10.0.2.0/24"
+  ]
+
+  private_subnet_cidrs = [
+    "10.0.11.0/24",
+    "10.0.12.0/24"
+  ]
+}
