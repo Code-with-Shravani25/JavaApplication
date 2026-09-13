7d6fe03a1b7ff3ab92ac88b165e53e1b5d10badc
Shravani Budharam <shravanibudharam25@gmail.com>
2026-09-13 21:05:45 +0530

    Create ECS Fargate task definition and service
    Added ECS Fargate task definition and service configuration.

--- terraform/modules/ecs/main.tf
+++ terraform/modules/ecs/main.tf
@@ -10,3 +10,83 @@ resource "aws_ecs_cluster" "this" {
     Project = var.project_name
   }
 }
+
+# ECS Fragate task definition
+resource "aws_ecs_task_definition" "this" {
+
+  family = "${var.project_name}-task"
+
+  requires_compatibilities = ["FARGATE"]
+
+  network_mode = "awsvpc"
+
+  cpu    = "256"
+  memory = "512"
+
+  execution_role_arn = var.execution_role_arn
+
+  container_definitions = jsonencode([
+    {
+      name  = "ecs-demo"
+      image = "${var.ecr_repository_url}:${var.image_tag}"
+
+      essential = true
+
+      portMappings = [
+        {
+          containerPort = 8080
+          protocol      = "tcp"
+        }
+      ]
+
+      logConfiguration = {
+        logDriver = "awslogs"
+
+        options = {
+          awslogs-group         = var.log_group_name
+          awslogs-region        = var.aws_region
+          awslogs-stream-prefix = "ecs"
+        }
+      }
+    }
+  ])
+}
+
+# ECS service with 2two tasks
+
+resource "aws_ecs_service" "this" {
+
+  name = "${var.project_name}-service"
+
+  cluster = aws_ecs_cluster.this.id
+
+  task_definition = aws_ecs_task_definition.this.arn
+
+  desired_count = 2
+
+  launch_type = "FARGATE"
+
+  network_configuration {
+
+    subnets = var.private_subnet_ids
+
+    security_groups = [
+      var.ecs_security_group_id
+    ]
+
+    assign_public_ip = false
+  }
+
+  load_balancer {
+
+    target_group_arn = var.target_group_arn
+
+    container_name = "ecs-demo"
+
+    container_port = 8080
+  }
+
+  depends_on = [
+    var.alb_listener_dependency
+  ]
+}
