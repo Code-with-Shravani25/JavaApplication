87007953a80d3bfc3f1900b3a1fbb9f4f2153798
Shravani Budharam <shravanibudharam25@gmail.com>
2026-09-13 21:10:26 +0530

    Add modules for ECR, IAM, Cloudwatch, ALB, and ECS

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,3 +18,58 @@ module "vpc" {
     "10.0.12.0/24"
   ]
 }
+
+# ECR
+module "ecr" {
+  source = "./modules/ecr"
+
+  project_name    = var.project_name
+  repository_name = "${var.project_name}-repo"
+}
+
+# IAM
+module "iam" {
+  source = "./modules/iam"
+
+  project_name = var.project_name
+}
+
+# Cloudwatch
+module "cloudwatch" {
+  source = "./modules/cloudwatch"
+
+  project_name = var.project_name
+}
+
+# ALB
+module "alb" {
+  source = "./modules/alb"
+
+  project_name = var.project_name
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.public_subnet_ids
+}
+
+# ECS
+module "ecs" {
+  source = "./modules/ecs"
+
+  project_name = var.project_name
+
+  image_tag = var.image_tag
+
+  ecr_repository_url = module.ecr.repository_url
+
+  private_subnet_ids = module.vpc.private_subnet_ids
+
+  execution_role_arn = module.iam.execution_role_arn
+
+  log_group_name = module.cloudwatch.log_group_name
+
+  target_group_arn = module.alb.target_group_arn
+
+  ecs_security_group_id = module.alb.ecs_security_group_id
+
+  aws_region = var.aws_region
+}
