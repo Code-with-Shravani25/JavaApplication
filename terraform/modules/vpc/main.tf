51c48447d382a1c5ecf4ec3656394b65c6634f2b
Shravani Budharam <shravanibudharam25@gmail.com>
2026-09-23 10:41:22 +0530

    Update main.tf

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -1,4 +1,7 @@
+# =========================
 # VPC
+# =========================
+
 resource "aws_vpc" "this" {
   cidr_block           = var.vpc_cidr
   enable_dns_support   = true
@@ -9,6 +12,11 @@ resource "aws_vpc" "this" {
   }
 }
 
+
+# =========================
+# Internet Gateway
+# =========================
+
 resource "aws_internet_gateway" "this" {
   vpc_id = aws_vpc.this.id
 
@@ -17,7 +25,11 @@ resource "aws_internet_gateway" "this" {
   }
 }
 
-# Create 2 public subnet
+
+# =========================
+# Public Subnets
+# =========================
+
 resource "aws_subnet" "public" {
   count = length(var.public_subnet_cidrs)
 
@@ -31,7 +43,11 @@ resource "aws_subnet" "public" {
   }
 }
 
-# Create 2 private subnet
+
+# =========================
+# Private Subnets
+# =========================
+
 resource "aws_subnet" "private" {
   count = length(var.private_subnet_cidrs)
 
@@ -44,3 +60,95 @@ resource "aws_subnet" "private" {
   }
 }
 
+
+# =========================
+# Public Route Table
+# =========================
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.this.id
+  }
+
+  tags = {
+    Name = "${var.project_name}-public-rt"
+  }
+}
+
+
+# =========================
+# Public Route Table Associations
+# =========================
+
+resource "aws_route_table_association" "public" {
+  count = length(var.public_subnet_cidrs)
+
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+
+# =========================
+# Elastic IP for NAT Gateway
+# =========================
+
+resource "aws_eip" "nat" {
+  domain = "vpc"
+
+  tags = {
+    Name = "${var.project_name}-nat-eip"
+  }
+}
+
+
+# =========================
+# NAT Gateway
+# =========================
+
+resource "aws_nat_gateway" "this" {
+  allocation_id = aws_eip.nat.id
+
+  # NAT Gateway MUST be in a PUBLIC subnet
+  subnet_id = aws_subnet.public[0].id
+
+  depends_on = [
+    aws_internet_gateway.this
+  ]
+
+  tags = {
+    Name = "${var.project_name}-nat"
+  }
+}
+
+
+# =========================
+# Private Route Table
+# =========================
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.this.id
+  }
+
+  tags = {
+    Name = "${var.project_name}-private-rt"
+  }
+}
+
+
+# =========================
+# Private Route Table Associations
+# =========================
+
+resource "aws_route_table_association" "private" {
+  count = length(var.private_subnet_cidrs)
+
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
+}
